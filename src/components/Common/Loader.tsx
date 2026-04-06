284453c73882210abe0b2b55049549252714f83f
Harshil Barad <90130954+Harshil-Barad@users.noreply.github.com>
2026-04-06 22:13:01 +0530

    Update Loader.tsx

--- src/components/Common/Loader.tsx
+++ src/components/Common/Loader.tsx
@@ -6,6 +6,6 @@ const Loader = () => {
       className={`ml-1.5 h-4 w-4 animate-spin rounded-full border-2 border-solid border-white border-t-transparent dark:border-t-transparent`}
     ></span>
   );
-}; 
+};  
 
 export default Loader;
